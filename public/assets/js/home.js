4e97fd194f15f609a48c2624a9720edfc5a0ef42
Myles Carey <seanmyles@comcast.net>
2019-10-02 21:13:31 -0700

    experimenting with various chart libraries

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -6,6 +6,13 @@ $(document).ready(function(){
 // make get routes call to retrieve a default model data 
 // and render it as table grid on page
 
+// // change page theme css style sheet
+//   function setStyleSheet(url) {
+//     var stylesheet = document.getElementById("css-theme");
+//     stylesheet.setAttribute('href',url);
+//   };
+
+
   // retreive servers for the order form control select box
   $.ajax("/api/plan-life-chapter/4", {
     type: "GET"
@@ -27,17 +34,218 @@ $(document).ready(function(){
         modelRow.append($(`<td>${chapter.inflation_pct}</td>`));
         $("#grid-table").append(modelRow);
       });
-    
-
-
-
-      // $("#server-selection").append('<option value="" selected="selected" hidden="hidden"  >Select server</option>');
-      // res.map(foodserver => {
-      //   $("#server-selection").append(`<option value="${foodserver.food_server_id}">${foodserver.food_server_name}</option`);
-      // });
     }
   );
 
+  // demo chart via chartist
+  var chartData = {
+    labels:[15,20,25,30,35,40,45,50,55,60,65],
+    series:[
+      {
+        name: 'model-1',
+        data: [200,500,600,3000,5000,6700,8000,11000,13000,20000,
+               22000],
+      },
+
+    ]
+  };
+
+
+
+
+
+// We are setting a few options for our chart and override the defaults
+var options = {
+  // Don't draw the line chart points
+  showPoint: true,
+  // Disable line smoothing
+  lineSmooth: true,
+  // X-Axis specific configuration
+  axisX: {
+    // We can disable the grid for this axis
+    showGrid: true,
+    // and also don't show the label
+    showLabel: true
+  },
+  // Y-Axis specific configuration
+  axisY: {
+    // Lets offset the chart a bit from the labels
+    offset: 60,
+    // The label interpolation function enables you to modify the values
+    // used for the labels on each axis. Here we are converting the
+    // values into million pound.
+    labelInterpolationFnc: function(value) {
+      return '$' + value;
+    }
+  }
+};
+
+  var myChart = new Chartist.Line('#demo-chart',chartData,options);
+
+  var nums = [];
+  var amount = 0;
+  var modelData = [];
+  for (let i = 15; i <= 66; i++) {
+    nums.push(i);
+    if (i % 5) {
+      modelData.push(amount += 500)
+    } else if (i % 3) {
+      modelData.push(amount += 5000)
+    } else if (i % 6) {
+      modelData.push(amount += 14000)
+    } else if (i % 7) {
+      modelData.push(amount += 30000)
+    } else {
+      modelData.push(amount += 2000)
+    };
+  };
+  console.log(nums);
+  console.log(modelData);
+
+  var title = "Kyra's 50 Year Retirement Plan";
+
+  Highcharts.setOptions({colors: ['#026873']});
+  Highcharts.chart('container', {
+    chart: {
+      type: 'area',
+      backgroundColor:  null 
+      // 'rgba(197, 209, 217, .2'
+    },
+    title: {
+      text: title 
+      // 'Historic and Estimated Worldwide Population Growth by Region'
+    },
+    // subtitle: {
+    //   text: 'Source: Wikipedia.org'
+    // },
+    xAxis: {
+      // categories: ['15', '16', '17', '18', '19', '20', '21'],
+      categories: nums,
+      // [15,16,17,18,19,20,21,22],
+      tickmarkPlacement: 'on',
+      title: {
+        enabled: false
+      }
+    },
+    yAxis: {
+      title: {
+        text: '$ Dollars'
+      }
+      // ,
+      // labels: {
+      //   formatter: function () {
+      //     return this.value / 1;
+      //   }
+      // }
+    },
+    tooltip: {
+      split: true,
+      valueSuffix: ' dollars'
+    },
+    plotOptions: {
+      series: {
+        marker: {
+          enabled: false
+        }
+      },
+      area: {
+        stacking: 'normal',
+        lineColor: '#666666',
+        lineWidth: 1,
+        marker: {
+          enabled: false
+          // lineWidth: 1,
+          // lineColor: '#666666'
+        }
+      }
+    },
+    line: {
+      marker: {
+          enabled: false
+      }
+    },
+    series: [{
+      name: 'Age',
+      data: modelData,
+      type: 'areaspline',
+      color: '#026873',
+      // [850, 900, 1100, 1400, 2200, 3800, 13000,25000]
+      fillColor: {
+        linearGradient: {
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 1
+        },
+        stops: [
+            [0, Highcharts.getOptions().colors[0]],
+            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+        ]
+    }
+    }
+    ]
+  });
+
+
+  // Highcharts.chart('container', {
+  //   chart: {
+  //     type: 'area'
+  //   },
+  //   title: {
+  //     text: 'Historic and Estimated Worldwide Population Growth by Region'
+  //   },
+  //   subtitle: {
+  //     text: 'Source: Wikipedia.org'
+  //   },
+  //   xAxis: {
+  //     categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
+  //     tickmarkPlacement: 'on',
+  //     title: {
+  //       enabled: false
+  //     }
+  //   },
+  //   yAxis: {
+  //     title: {
+  //       text: 'Billions'
+  //     },
+  //     labels: {
+  //       formatter: function () {
+  //         return this.value / 1000;
+  //       }
+  //     }
+  //   },
+  //   tooltip: {
+  //     split: true,
+  //     valueSuffix: ' millions'
+  //   },
+  //   plotOptions: {
+  //     area: {
+  //       stacking: 'normal',
+  //       lineColor: '#666666',
+  //       lineWidth: 1,
+  //       marker: {
+  //         lineWidth: 1,
+  //         lineColor: '#666666'
+  //       }
+  //     }
+  //   },
+  //   series: [{
+  //     name: 'Asia',
+  //     data: [502, 635, 809, 947, 1402, 3634, 5268]
+  //   }, {
+  //     name: 'Africa',
+  //     data: [106, 107, 111, 133, 221, 767, 1766]
+  //   }, {
+  //     name: 'Europe',
+  //     data: [163, 203, 276, 408, 547, 729, 628]
+  //   }, {
+  //     name: 'America',
+  //     data: [18, 31, 54, 156, 339, 818, 1201]
+  //   }, {
+  //     name: 'Oceania',
+  //     data: [2, 2, 2, 6, 13, 30, 46]
+  //   }]
+  // });
 
 
 });
