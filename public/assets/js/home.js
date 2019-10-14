3c2bdcc7b3bc66ae8e44bf3b004e08ecd6526929
Myles Carey <seanmyles@comcast.net>
2019-10-13 20:29:43 -0700

    built login-signup-logout modal

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1028,6 +1028,37 @@ $(document).ready(function(){
   
   });
 
+
+  // login drop down event
+  $(document).on("click", ".dropdown-item.signin", function() {
+    event.preventDefault();
+    console.log("in global.dropdown-item.signin click event");
+    console.log("you pressed: " + $(this).data("value"));
+    var clickedValue = $(this).data("value");
+    console.log("value is: ",clickedValue); 
+    if (clickedValue === 'login') {
+      $('#submit').val('Login');
+      $('#signup-msg').show();
+      $('#login-modal').modal('show');
+    } else if (clickedValue === 'signup') {
+      $('#submit').val('Signup');
+      $('#signup-msg').hide();
+      $('#login-modal').modal('show');
+    } else {
+      console.log("this is were we logout"); 
+      $('#logout-modal').modal('show');
+
+    };
+  });
+
+    // jump from login to signup
+    $(document).on("click", "#jump-to-signup", function() {
+      console.log("in global.jump-to-signup click event");
+      $('#submit').val('Signup');
+      $('#signup-msg').hide();
+    });
+
+
   // toggle upper section on/off when users scrolls down/up
   window.onscroll = function() {hideDiv()};
 
@@ -1042,251 +1073,5 @@ $(document).ready(function(){
     }
   };
 
-  // ----------------------------------------------------------
-  // START OF PROGRAM FLOW:
-  // ----------------------------------------------------------
-  // console.log("In Home Page");
-  // var activeWallpaper = manageLocalStorage.getLocalStorage('wallpaper');
-  // if (!activeWallpaper) {
-  //   activeWallpaper ='money'
-  // };
-
-  // setStyleSheet(`assets/css/${activeWallpaper}-theme.css`);
   
-
-  // SAVING CODE that processed API results when they weren't converted to Class objects
-  // $.ajax("/api/plan-user-life-chapter/4", {
-  //   type: "GET"
-  // }).then(function(res) {
-  //     console.log(res);
-  //     console.log(`plan name: ${res.plan_name}`);
-  //     $("#grid-caption").text(res.plan_name);
-  //     res.LifeChapters.map(chapter => {
-  //       console.log(`seq: ${chapter.seq_no} name ${chapter.chapter_name} start ${chapter.start_age} end ${chapter.end_age} 
-  //                    invest-amt ${chapter.invest_amount} frequency: ${chapter.InvestRateType.invest_type} 
-  //                    return-rate ${chapter.return_pct} inflation-rate ${chapter.inflation_pct}`);
-  //       var modelRow = $('<tr>');
-  //       modelRow.append($(`<td>${chapter.chapter_name}</td>`));
-  //       modelRow.append($(`<td>${chapter.start_age}</td>`));
-  //       modelRow.append($(`<td>${chapter.end_age}</td>`));
-  //       modelRow.append($(`<td>${chapter.invest_amount}</td>`));
-  //       modelRow.append($(`<td>${chapter.InvestRateType.invest_type}</td>`));
-  //       modelRow.append($(`<td>${chapter.return_pct}</td>`));
-  //       modelRow.append($(`<td>${chapter.inflation_pct}</td>`));
-  //       $("#grid-table").append(modelRow);
-  //     });
-  //   }
-  // );
-
-  // demo chart via chartist
-  // var chartData = {
-  //   labels:[15,20,25,30,35,40,45,50,55,60,65],
-  //   series:[
-  //     {
-  //       name: 'model-1',
-  //       data: [200,500,600,3000,5000,6700,8000,11000,13000,20000,
-  //              22000],
-  //     },
-
-  //   ]
-  // };
-
-
-
-
-
-// // We are setting a few options for our chart and override the defaults
-// var options = {
-//   // Don't draw the line chart points
-//   showPoint: true,
-//   // Disable line smoothing
-//   lineSmooth: true,
-//   // X-Axis specific configuration
-//   axisX: {
-//     // We can disable the grid for this axis
-//     showGrid: true,
-//     // and also don't show the label
-//     showLabel: true
-//   },
-//   // Y-Axis specific configuration
-//   axisY: {
-//     // Lets offset the chart a bit from the labels
-//     offset: 60,
-//     // The label interpolation function enables you to modify the values
-//     // used for the labels on each axis. Here we are converting the
-//     // values into million pound.
-//     labelInterpolationFnc: function(value) {
-//       return '$' + value;
-//     }
-//   }
-// };
-
-  // var myChart = new Chartist.Line('#demo-chart',chartData,options);
-
-  // var nums = [];
-  // var amount = 0;
-  // var modelData = [];
-  // for (let i = 15; i <= 66; i++) {
-  //   nums.push(i);
-  //   if (i % 5) {
-  //     modelData.push(amount += 200 + i * 50)
-  //   } else if (i % 3) {
-  //     modelData.push(amount += 400 + i * 100)
-  //   } else if (i % 6) {
-  //     modelData.push(amount += 1000 + i * 200)
-  //   } else if (i % 7) {
-  //     modelData.push(amount += 800 + i * 300)
-  //   } else {
-  //     modelData.push(amount += 600 + i * 20)
-  //   };
-  // };
-  // console.log(nums);
-  // console.log(modelData);
-
-  
-
-  // Highcharts.setOptions({colors: ['#026873']});
-  // Highcharts.chart('container', {
-  //   chart: {
-  //     type: 'area',
-  //     backgroundColor:  null 
-  //     // 'rgba(197, 209, 217, .2'
-  //   },
-  //   title: {
-  //     text: title 
-  //     // 'Historic and Estimated Worldwide Population Growth by Region'
-  //   },
-  //   // subtitle: {
-  //   //   text: 'Source: Wikipedia.org'
-  //   // },
-  //   xAxis: {
-  //     // categories: ['15', '16', '17', '18', '19', '20', '21'],
-  //     categories: nums,
-  //     // [15,16,17,18,19,20,21,22],
-  //     tickmarkPlacement: 'on',
-  //     title: {
-  //       enabled: false
-  //     }
-  //   },
-  //   yAxis: {
-  //     title: {
-  //       text: '$ Dollars'
-  //     }
-  //     // ,
-  //     // labels: {
-  //     //   formatter: function () {
-  //     //     return this.value / 1;
-  //     //   }
-  //     // }
-  //   },
-  //   tooltip: {
-  //     split: true,
-  //     valueSuffix: ' dollars'
-  //   },
-  //   plotOptions: {
-  //     series: {
-  //       marker: {
-  //         enabled: false
-  //       }
-  //     },
-  //     area: {
-  //       stacking: 'normal',
-  //       lineColor: '#666666',
-  //       lineWidth: 1,
-  //       marker: {
-  //         enabled: false
-  //         // lineWidth: 1,
-  //         // lineColor: '#666666'
-  //       }
-  //     }
-  //   },
-  //   line: {
-  //     marker: {
-  //         enabled: false
-  //     }
-  //   },
-  //   series: [{
-  //     name: 'Age',
-  //     data: modelData,
-  //     type: 'areaspline',
-  //     color: '#026873',
-  //     // [850, 900, 1100, 1400, 2200, 3800, 13000,25000]
-  //     fillColor: {
-  //       linearGradient: {
-  //           x1: 0,
-  //           y1: 0,
-  //           x2: 0,
-  //           y2: 1
-  //       },
-  //       stops: [
-  //           [0, Highcharts.getOptions().colors[0]],
-  //           [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-  //       ]
-  //   }
-  //   }
-  //   ]
-  // });
-
-
-  // Highcharts.chart('container', {
-  //   chart: {
-  //     type: 'area'
-  //   },
-  //   title: {
-  //     text: 'Historic and Estimated Worldwide Population Growth by Region'
-  //   },
-  //   subtitle: {
-  //     text: 'Source: Wikipedia.org'
-  //   },
-  //   xAxis: {
-  //     categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
-  //     tickmarkPlacement: 'on',
-  //     title: {
-  //       enabled: false
-  //     }
-  //   },
-  //   yAxis: {
-  //     title: {
-  //       text: 'Billions'
-  //     },
-  //     labels: {
-  //       formatter: function () {
-  //         return this.value / 1000;
-  //       }
-  //     }
-  //   },
-  //   tooltip: {
-  //     split: true,
-  //     valueSuffix: ' millions'
-  //   },
-  //   plotOptions: {
-  //     area: {
-  //       stacking: 'normal',
-  //       lineColor: '#666666',
-  //       lineWidth: 1,
-  //       marker: {
-  //         lineWidth: 1,
-  //         lineColor: '#666666'
-  //       }
-  //     }
-  //   },
-  //   series: [{
-  //     name: 'Asia',
-  //     data: [502, 635, 809, 947, 1402, 3634, 5268]
-  //   }, {
-  //     name: 'Africa',
-  //     data: [106, 107, 111, 133, 221, 767, 1766]
-  //   }, {
-  //     name: 'Europe',
-  //     data: [163, 203, 276, 408, 547, 729, 628]
-  //   }, {
-  //     name: 'America',
-  //     data: [18, 31, 54, 156, 339, 818, 1201]
-  //   }, {
-  //     name: 'Oceania',
-  //     data: [2, 2, 2, 6, 13, 30, 46]
-  //   }]
-  // });
-
-
 });
